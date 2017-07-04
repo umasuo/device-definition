df47e5a35439b7fdd9bb032c5eaa2b9436b7c446
davis999 <davis.dai@go6d.com>
2017-07-04 11:25:32 +0800

    add redis

--- src/main/java/com/umasuo/device/definition/application/service/ProductTypeApplication.java
+++ src/main/java/com/umasuo/device/definition/application/service/ProductTypeApplication.java
@@ -1,18 +1,22 @@
 package com.umasuo.device.definition.application.service;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.umasuo.device.definition.application.dto.CommonDataView;
 import com.umasuo.device.definition.application.dto.ProductTypeView;
 import com.umasuo.device.definition.application.dto.mapper.ProductTypeMapper;
 import com.umasuo.device.definition.domain.model.ProductType;
 import com.umasuo.device.definition.domain.service.ProductTypeService;
+import com.umasuo.device.definition.infrastructure.util.RedisUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Davis on 17/6/28.
@@ -38,28 +42,51 @@ public class ProductTypeApplication {
   @Autowired
   private transient RestClient restClient;
 
+  @Autowired
+  private transient RedisTemplate redisTemplate;
+
   /**
    * 查询所有的产品类型。
    */
   public List<ProductTypeView> getAll() {
     LOG.debug("Enter.");
 
-    List<ProductType> productTypes = productTypeService.getAll();
+    // 1. get from redis
+    Map<String, Object> cacheProductTypes =
+        redisTemplate.opsForHash().entries(RedisUtils.PRODUCT_TYPE_KEY);
 
-    List<String> dataDefinitionIds = getDataDefinitionIds(productTypes);
+    if (cacheProductTypes == null || cacheProductTypes.isEmpty()) {
+      // 2.1 get from db
 
-    List<CommonDataView> dataDefinitionViews = Lists.newArrayList();
+      // 2.2 insert into redis
 
-    if (!dataDefinitionIds.isEmpty()) {
-      // 调用data-definition的api获取对应id的CommonDataView
-      dataDefinitionViews = restClient.getPlatformDataDefinition(dataDefinitionIds);
+      List<ProductType> productTypes = productTypeService.getAll();
+
+      List<String> dataDefinitionIds = getDataDefinitionIds(productTypes);
+
+      List<CommonDataView> dataDefinitionViews = Lists.newArrayList();
+
+      if (!dataDefinitionIds.isEmpty()) {
+        // 调用data-definition的api获取对应id的CommonDataView
+        dataDefinitionViews = restClient.getPlatformDataDefinition(dataDefinitionIds);
+      }
+
+      List<ProductTypeView> result = ProductTypeMapper.toModel(productTypes, dataDefinitionViews);
+
+      cacheProductTypes = Maps.newHashMap();
+
+      Map<String, Object> finalCacheProductTypes = cacheProductTypes;
+      result.stream().forEach(view -> finalCacheProductTypes.put(view.getId(), view));
+      redisTemplate.opsForHash().putAll(RedisUtils.PRODUCT_TYPE_KEY, finalCacheProductTypes);
+
+      LOG.debug("Exit. productType size: {}.", result.size());
+
+      return result;
+    } else {
+      List<ProductTypeView> result = Lists.newArrayList();
+      cacheProductTypes.entrySet().stream().forEach( entrySet -> result.add((ProductTypeView) entrySet.getValue()));
+      return result;
     }
-
-    List<ProductTypeView> result = ProductTypeMapper.toModel(productTypes, dataDefinitionViews);
-
-    LOG.debug("Exit. productType size: {}.", result.size());
-
-    return result;
   }
 
   /**
---
+++ src/main/java/com/umasuo/device/definition/infrastructure/util/RedisUtils.java
@@ -0,0 +1,9 @@
+package com.umasuo.device.definition.infrastructure.util;
+
+/**
+ * Created by Davis on 17/7/4.
+ */
+public class RedisUtils {
+
+  public static final String PRODUCT_TYPE_KEY = "product:producttype";
+}
