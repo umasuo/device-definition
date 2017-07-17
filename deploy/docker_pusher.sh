8d58aeab7fad513dc6d0806f42d7f77f6dea29f4
davis999 <davis.dai@go6d.com>
2017-07-17 17:49:35 +0800

    fix docker bug

--- deploy/docker_pusher.sh
+++ deploy/docker_pusher.sh
@@ -13,6 +13,6 @@ echo ${BRANCH}
 echo "====================================================================="
 if [ "$BRANCH" = "master" ] || [ "$BRANCH" = "dev" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD";
-  docker push umasuo/device-definition;
+  docker push umasuo/product;
 fi
 echo "============= end push docker image ================================="
